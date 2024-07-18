64fcad505327cdc415bbc59fe54576bcee433ff1
Alexandre Careil <alex.careil.fr@gmail.com>
2024-07-18 10:39:45 +0200

    display identity in leaderboard list

--- vibe-check-frontend/src/LeaderBoard.vue
+++ vibe-check-frontend/src/LeaderBoard.vue
@@ -8,11 +8,23 @@ const identity = getWebAuthnIdentity();
 
 const sortedBalances = computed(() => {
     return Object.entries(SmileTokenBalances.value)
-        .filter(b => b[0] !== identity)
         .sort((a, b) => b[1] - a[1]);
 });
 
-const identityBalance = Object.entries(SmileTokenBalances.value).find(([balanceId, _]) => balanceId === identity);
+const progress_bar = (score: number, max: number, nb_bars = 15) => {
+    const current_percent = score / max * nb_bars;
+    let res = "";
+	for (let n = 1; n < nb_bars + 1; n++) {
+        if (current_percent < n) {
+            res += "░"; // alt-176 
+        }
+        else {
+            res += "▓"; // alt-178
+        }
+	}
+
+    return res;
+}
 
 
 </script>
@@ -21,20 +33,47 @@ const identityBalance = Object.entries(SmileTokenBalances.value).find(([balanceI
     <div class="container m-auto">
         <h1 class="text-center my-4">Vibe Check</h1>
         <h3 class="text-center my-4"><img :src="Logo" alt="Hylé logo" class="h-10 m-auto"></img></h3>
-        <strong id="address">{{ identity }}</strong>
-        <div>
-            <p v-if="!!identityBalance" :key="identityBalance[0]"><strong>{{ identityBalance[0] }}: {{ identityBalance[1] }}</strong></p>
-            <p v-for="balance in sortedBalances" :key="balance[0]">{{ balance[0] }}: {{ balance[1] }}</p>
-        </div>
+        <strong id="address" class="identity">{{ identity }}</strong>
+        <hr />
+        <h1 class="leaderboard_title">Leaderboard</h1>
+        <hr />
+        <ul class="results">
+            <li v-for="(balance, index) in sortedBalances" :key="balance[0]" :class="balance[0] === identity ? 'identity' : 'else'"><strong>#{{ index + 1 }}</strong> {{ balance[0] }} - {{ balance[1] }} {{ progress_bar(balance[1], 200) }}</li>
+        </ul>
     </div>
 </template>
 
 <style>
+
+.leaderboard_title {
+    text-align: center;
+    margin: 0.5em;
+}
+
+.results {
+    margin: auto;
+    text-align: center;
+    margin-top: 1em;
+}
+
+.identity {
+    background-color:white;
+    color: #E0482E;
+}
+
+ul > li {
+    font-family: monospace;
+    list-style: none;
+    padding: 0.5em;
+    font-size: 1.2em;
+}
+
 #address {
+    font-family: monospace;
     position: absolute;
     top: 0;
     right: 0;
-    padding: 0;
+    padding: 0.5em;
     margin: 0;
   }
 </style>
