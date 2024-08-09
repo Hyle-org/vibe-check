01d587f0be2cbd8010ee1c3174c69ecce631ed39
Lancelot de Ferrière <wraitii@gmail.com>
2024-08-09 17:04:27 +0200

    Pessimise prover.ts for vite/rollup

--- vibe-check-frontend/src/smart_contracts/cairo/prover.ts
+++ vibe-check-frontend/src/smart_contracts/cairo/prover.ts
@@ -1,24 +1,25 @@
 import { CairoArgs, CairoSmileArgs } from "@/smart_contracts/SmartContract";
 
 function viaWorker(
-    file: string,
-    generator: (worker: Worker, resolve: (value: Uint8Array) => void) => void,
+    // I can't create the worker here or it confuses rollup and we end up with the wrong path.
+    worker: Worker,
+    generator: (resolve: (value: Uint8Array) => void) => void,
 ): Promise<any> {
-    const worker = new Worker(new URL(file, import.meta.url), {
-        type: "module",
-    });
     return new Promise((resolve, reject) => {
         worker.onerror = (e) => {
             console.error(e);
             worker.terminate();
             reject(e);
         };
-        generator(worker, resolve);
+        generator(resolve);
     });
 }
 
 export const proveERC20Transfer = (args: CairoArgs): Promise<Uint8Array> => {
-    return viaWorker("./CairoRunner.ts", (worker, resolve) => {
+    const worker = new Worker(new URL("./CairoRunner.ts", import.meta.url), {
+        type: "module",
+    });
+    return viaWorker(worker, (resolve) => {
         worker.onmessage = (e) => {
             resolve(e.data.proof);
             worker.terminate();
@@ -29,7 +30,10 @@ export const proveERC20Transfer = (args: CairoArgs): Promise<Uint8Array> => {
 };
 
 export const runSmile = (args: CairoSmileArgs): Promise<string> => {
-    return viaWorker("./CairoRunner.ts", (worker, resolve) => {
+    const worker = new Worker(new URL("./CairoRunner.ts", import.meta.url), {
+        type: "module",
+    });
+    return viaWorker(worker, (resolve) => {
         worker.onmessage = (e) => {
             resolve(e.data[1].output);
             worker.terminate();
@@ -39,7 +43,10 @@ export const runSmile = (args: CairoSmileArgs): Promise<string> => {
 };
 
 export const proveSmile = (args: CairoSmileArgs): Promise<Uint8Array> => {
-    return viaWorker("./CairoRunner.ts", (worker, resolve) => {
+    const worker = new Worker(new URL("./CairoRunner.ts", import.meta.url), {
+        type: "module",
+    });
+    return viaWorker(worker, (resolve) => {
         worker.onmessage = (e) => {
             if (e.data[0] === "smile-proof") {
                 resolve(e.data[1].proof);
