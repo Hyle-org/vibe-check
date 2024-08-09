6e9e4b14d3367d25c23c45781a84c4b7f54c948e
Alexandre Careil <alex.careil.fr@gmail.com>
2024-08-09 17:01:07 +0200

    extract and validate config

--- cairo-proving-server/src/logger.rs
+++ cairo-proving-server/src/logger.rs
@@ -1,3 +1,6 @@
+use anyhow::Context;
+use log::{error, warn};
+
 pub fn setup_logger() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
         .format(|out, message, record| {
@@ -13,4 +16,30 @@ pub fn setup_logger() -> Result<(), fern::InitError> {
         .chain(fern::log_file("output.log")?)
         .apply()?;
     Ok(())
+}
+
+// A simple way to log without interrupting fluency
+pub trait LogMe<T> {
+    fn log_warn(self, context_msg: String) -> anyhow::Result<T>;
+    fn log_error(self, context_msg: String) -> anyhow::Result<T>;
+}
+
+// Will log a warning in case of error
+// WARN {context_msg}: {cause}
+impl<T, Error: std::error::Error + Send + Sync + 'static> LogMe<T> for Result<T, Error> {
+    fn log_warn(self, context_msg: String) -> anyhow::Result<T> {
+        let res = self.context(context_msg);
+        if let Err(e) = &res {
+            warn!("{:#}", e);
+        }
+        res
+    }
+
+    fn log_error(self, context_msg: String) -> anyhow::Result<T> {
+        let res = self.context(context_msg);
+        if let Err(e) = &res {
+            error!("{:#}", e);
+        }
+        res
+    }
 }
