c353dfe2a72a51339fdf03d0b4523a06025846e7
Lancelot de Ferrière <wraitii@gmail.com>
2024-09-04 15:47:59 +0200

    Fixups for deployment

--- noir-proving-server/src/server.ts
+++ noir-proving-server/src/server.ts
@@ -1,22 +1,29 @@
-import express, { Request, Response } from 'express';
-import { proveECDSA } from './prover';
-import cors from 'cors';
+import express, { Request, Response } from "express";
+import { proveECDSA } from "./prover";
+import cors from "cors";
 
 const app = express();
 
-app.use(express.raw({ type: 'application/octet-stream', limit: 'Infinity' }));
+app.use(express.raw({ type: "application/octet-stream", limit: "Infinity" }));
 app.use(cors());
 
-app.post('/prove-ecdsa', async (req: Request, res: Response) => {
-    console.log("Received a prove request")
+const router = express.Router();
+router.get("/health", async (req: Request, res: Response) => {
+    res.status(200).send("ok");
+});
+
+router.post("/prove-ecdsa", async (req: Request, res: Response) => {
+    console.log("Received a prove request");
     try {
         const result = await proveECDSA(req.body);
         res.status(200).send(result);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Une erreur est survenue');
+        res.status(500).send("Une erreur est survenue");
     }
 });
+
+app.use(process.env.PREFIX || "/", router);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
